789698a4581c9818a0686aeb16d809d00efd33c7
DuyJohannes0870 <duy.hpk0870@sinhvien.hoasen.edu.vn>
2021-10-24 12:28:04 +0700

    Love

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bé Yêu';
+const giftUrl = 'https://www.facebook.com/Duy.HPK/';
+const eventName = 'Chúc Mừng Kỷ Niệm';
+const titleCard = 'Tặng người Yêu';
+const contentCard = 'Suy cho cùng thì, Cuộc đời này: Tiền tài, Địa vị, Tất cả mọi thứ sắp có. Cũng không bằng Tình cảm Chúng mình bé ạ. Anh yêu Bé Thy ❤ 1505 ';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
